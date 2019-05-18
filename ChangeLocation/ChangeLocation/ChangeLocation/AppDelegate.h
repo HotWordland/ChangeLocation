2100315f06a0a12b037fb4492f7f320b03cd607d
文明 <740747055@qq.com>
2019-05-18 23:32:51 +0800

    Add files via upload

---
+++ ChangeLocation/ChangeLocation/ChangeLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChangeLocation
+//
+//  Created by wenmingzheng on 2018/1/26.
+//  Copyright © 2018年 wenmingzheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
